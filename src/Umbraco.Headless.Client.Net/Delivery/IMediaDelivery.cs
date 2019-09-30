0ae1da8c8f076d0512ba83122f5f1bb14f5a9dcf
Morten Christensen <mc@sitereactor.dk>
2019-09-30 10:51:50 +0200

    Implementing additional test cases for Media Delivery

--- src/Umbraco.Headless.Client.Net/Delivery/IMediaDelivery.cs
+++ src/Umbraco.Headless.Client.Net/Delivery/IMediaDelivery.cs
@@ -11,7 +11,7 @@ namespace Umbraco.Headless.Client.Net.Delivery
         Task<IEnumerable<T>> GetRoot<T>() where T : IContentBase;
         Task<Media> GetById(Guid id);
         Task<T> GetById<T>(Guid id) where T : IContentBase;
-        Task<PagedMedia> GetChildren(Guid id, int page = 0, int pageSize = 10);
-        Task<PagedMedia<T>> GetChildren<T>(Guid id, int page = 0, int pageSize = 10) where T : IContentBase;
+        Task<PagedMedia> GetChildren(Guid id, int page = 1, int pageSize = 10);
+        Task<PagedMedia<T>> GetChildren<T>(Guid id, int page = 1, int pageSize = 10) where T : IContentBase;
     }
 }
--- test/Umbraco.Headless.Client.Net.Tests/MediaDeliveryFixture.cs
+++ test/Umbraco.Headless.Client.Net.Tests/MediaDeliveryFixture.cs
@@ -1,5 +1,11 @@
-﻿using RichardSzalay.MockHttp;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RichardSzalay.MockHttp;
 using Umbraco.Headless.Client.Net.Configuration;
+using Umbraco.Headless.Client.Net.Delivery;
+using Xunit;
 
 namespace Umbraco.Headless.Client.Net.Tests
 {
@@ -12,6 +18,66 @@ namespace Umbraco.Headless.Client.Net.Tests
         public MediaDeliveryFixture()
         {
             _mockHttp = new MockHttpMessageHandler();
+        }
+
+        [Fact]
+        public async Task Can_Retrieve_Root_Media()
+        {
+            var service = new ContentDeliveryService(_configuration,
+                GetMockedHttpClient($"{_mediaBaseUrl}", MediaDeliveryJson.GetRoot));
+            var mediaItems = await service.Media.GetRoot();
+            Assert.NotNull(mediaItems);
+            Assert.NotEmpty(mediaItems);
+            Assert.Equal(4, mediaItems.Count());
+        }
+
+        [Theory]
+        [InlineData("6d986832-fb11-4d65-b2ae-0d7742f27a19")]
+        public async Task Can_Retrieve_Media_Folder_By_Id(string id)
+        {
+            var mediaId = Guid.Parse(id);
+            var service = new ContentDeliveryService(_configuration,
+                GetMockedHttpClient($"{_mediaBaseUrl}/*", MediaDeliveryJson.GetMediaByIdFolder));
+            var media = await service.Media.GetById(mediaId);
+            Assert.NotNull(media);
+            Assert.NotEmpty(media.Properties);
+            Assert.Equal(1, media.Properties.Count);
+        }
+
+        [Theory]
+        [InlineData("3c485159-99a6-44e4-a5f4-576253e0fda5")]
+        public async Task Can_Retrieve_Media_Item_By_Id(string id)
+        {
+            var mediaId = Guid.Parse(id);
+            var service = new ContentDeliveryService(_configuration,
+                GetMockedHttpClient($"{_mediaBaseUrl}/*", MediaDeliveryJson.GetMediaByIdMediaItem));
+            var media = await service.Media.GetById(mediaId);
+            Assert.NotNull(media);
+            Assert.NotEmpty(media.Properties);
+            Assert.Equal(6, media.Properties.Count);
+        }
+
+        [Theory]
+        [InlineData("6d986832-fb11-4d65-b2ae-0d7742f27a19")]
+        public async Task Can_Retrieve_Children_By_ParentId(string id)
+        {
+            var contentId = Guid.Parse(id);
+            var service = new ContentDeliveryService(_configuration,
+                GetMockedHttpClient($"{_mediaBaseUrl}/{id}/children", MediaDeliveryJson.GetChildrenByParentId));
+            var children = await service.Media.GetChildren(contentId);
+            Assert.NotNull(children);
+            Assert.NotNull(children.Media);
+            Assert.NotEmpty(children.Media.Items);
+            Assert.Equal(1, children.TotalResults);
+            Assert.Equal(1, children.TotalPages);
+            Assert.Equal(1, children.Page);
+        }
+
+        private HttpClient GetMockedHttpClient(string url, string jsonResponse)
+        {
+            _mockHttp.When(url).Respond("application/json", jsonResponse);
+            var client = new HttpClient(_mockHttp) { BaseAddress = new Uri(Constants.Urls.BaseCdnUrl) };
+            return client;
         }
     }
 }
