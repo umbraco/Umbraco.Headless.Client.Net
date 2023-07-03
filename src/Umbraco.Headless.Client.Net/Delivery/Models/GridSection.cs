f7e3c17a083e5929add0217012885d7f54d94e36
Phil Bacon <Phil-B@users.noreply.github.com>
2023-07-03 09:50:54 +0100

    Added support for grid editor layout (#62)

---
+++ src/Umbraco.Headless.Client.Net/Delivery/Models/GridSection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Umbraco.Headless.Client.Net.Delivery.Models
+{
+    public class GridSection
+    {
+        public int Grid { get; set; }
+        public IEnumerable<GridRow> Rows { get; set; }
+    }
+}
